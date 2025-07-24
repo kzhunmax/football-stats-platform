fbd31f3b8f274cec53ca3e9d5296c7041793e83d
Maksym Korshun <korshunmax80@gmail.com>
2025-07-24 22:07:16 +0200

    Init project setup with Spring Boot

---
+++ src/main/java/com/github/kzhunmax/footballstatsplatform/FootballStatsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.github.kzhunmax.footballstatsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballStatsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballStatsPlatformApplication.class, args);
+	}
+
+}
