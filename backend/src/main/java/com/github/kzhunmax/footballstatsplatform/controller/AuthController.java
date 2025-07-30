e668d45f6fb0ff72117e8d716b3979aa2e362d2f
Maksym Korshun <korshunmax80@gmail.com>
2025-07-30 18:29:13 +0200

    Add API controllers - Implemented REST endpoints - Configured request mappings

---
+++ backend/src/main/java/com/github/kzhunmax/footballstatsplatform/controller/AuthController.java
@@ -0,0 +1,72 @@
+package com.github.kzhunmax.footballstatsplatform.controller;
+
+import com.github.kzhunmax.footballstatsplatform.dto.request.UserLoginDTO;
+import com.github.kzhunmax.footballstatsplatform.dto.request.UserRegistrationDTO;
+import com.github.kzhunmax.footballstatsplatform.dto.response.JwtResponse;
+import com.github.kzhunmax.footballstatsplatform.dto.response.UserResponseDTO;
+import com.github.kzhunmax.footballstatsplatform.payload.ApiResponse;
+import com.github.kzhunmax.footballstatsplatform.security.JwtService;
+import com.github.kzhunmax.footballstatsplatform.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@Slf4j
+public class AuthController {
+    private final AuthService authService;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    private final AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<UserResponseDTO>> registerUser(
+            @Valid @RequestBody UserRegistrationDTO userRegistrationDTO) {
+
+        String requestId = UUID.randomUUID().toString();
+        log.info("Processing registration request | requestId={}, username={}", requestId, userRegistrationDTO.username());
+
+        UserResponseDTO user = authService.registerUser(userRegistrationDTO, requestId);
+        log.info("Successfully registered user | requestId={} username={}", requestId, user.username());
+        return ApiResponse.created(user, requestId);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<JwtResponse>> login(@Valid @RequestBody UserLoginDTO loginDto, HttpServletRequest request) {
+        String requestId = UUID.randomUUID().toString();
+        log.info("Login attempt | requestId={}, username={}", requestId, loginDto.usernameOrEmail());
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.usernameOrEmail(),
+                        loginDto.password()
+                )
+        );
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String jwtToken = jwtService.generateToken(userDetails);
+
+        log.info("Successful login | requestId={}, username={}", requestId, userDetails.getUsername());
+        return ApiResponse.success(new JwtResponse(jwtToken), requestId);
+    }
+}
